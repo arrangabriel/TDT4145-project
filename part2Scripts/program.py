e028f4e6e63ab842f9ead204202aa8adb4a2e2e9
Vetle Roos Mangrud <vmangrud@gmail.com>
2022-03-22 22:29:34 +0100

    Add functionality to add review!

--- part2Scripts/program.py
+++ part2Scripts/program.py
@@ -1,21 +1,23 @@
 import sqlite3
+from datetime import datetime
+
 con = sqlite3.connect("test.db")
 cursor = con.cursor()
-currentUser = ""
+currentUser = None
 
 def login():
     email = input("Email: ")
     password = input("Password: ")
     
     # Check if correct password
-    cursor.execute(f"SELECT * FROM User WHERE email='{email}' AND password='{password}'")
+    cursor.execute("SELECT email, firstname, lastname FROM User WHERE email=? AND password=?", [email, password])
     userMatch = cursor.fetchone()
     if(userMatch == None):
         return False
 
     # Sets currentUser to the logged in user
     global currentUser
-    currentUser = userMatch
+    currentUser = {"email": userMatch[0], "firstName": userMatch[1], "lastName": userMatch[2]}
     return True
     
 
@@ -27,26 +29,40 @@ def review():
     # roastery, coffee name, points and notes
     roastery = input("Roastery: ")
     coffeeName = input("Coffee Name: ")
-    points = int(input("Points: "))
-    notes = input("Notes: ")
 
     # Check if roast exists
-    cursor.execute(f"SELECT * FROM Roast WHERE roastery = '{roastery}' AND name = '{coffeeName}")
-    cursor.execute(f"")
+    cursor.execute(f"SELECT * FROM Roast WHERE roastery = ? AND name = ?", [roastery, coffeeName])
+    if(cursor.fetchone() == None):
+        print("Could not find roast")
+        return False
+
+    #Check if points is an integer
+    try:
+        points = int(input("Points: "))
+    except:
+        print("Points must be an integer")
+        return False
+    notes = input("Notes: ")
+
+    # Add to database    
+    cursor.execute("INSERT INTO Tasting(userEmail, roastery, roastName, note, points, date) VALUES (?,?,?,?,?,?)",[currentUser["email"], roastery, coffeeName, notes, points, datetime.now()])
+    con.commit()
 
 def main():
-    print("Welcome to")
+    print("Welcome to CoffeeDB")
     while not login():
         print("Could not find user. Please try again")
-    print(f"Welcome")
+    print(f"Welcome {currentUser['firstName']} {currentUser['lastName']}")
     choices = {
         "v": view,
-        "r": review
+        "r": review,
+        "e": end
     }
-    choice = ""
-    while not choice in choices.keys():
-        choice = input("What do you want to do?\n[v]iew coffees | add [r]eview: ")
-    choices[choice]()
+    while True:
+        choice = ""
+        while not choice in choices.keys():
+            choice = input("What do you want to do?\n[v]iew coffees | add [r]eview | [e]nd program: ")
+        choices[choice]()
 
 def end():
     con.close()
